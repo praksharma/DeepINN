d008d61502f4d2c24994496fce3f4a79b801a2f0
Prakhar Sharma <prakhar.sharma@ukaea.uk>
2026-08-04 22:38:34 +0100

    starting to rename the project

--- DeepINN/backend/__init__.py
+++ DeepINN/backend/__init__.py
@@ -1,4 +1,4 @@
-# A fork from DeepXDE.
+# Based on DeepXDE.
 # The backend is used to use different ML langauges.
 # Link: https://github.com/dmlc/dgl/tree/master/python/dgl/backend
 # Here we are only implementing PyTorch
@@ -7,21 +7,25 @@ import sys
 from .utils import loss_metric, choose_optimiser
 from ..__about__ import __version__
 
-ascii_art = """>>================================================<<
-||######                       ### #     # #     #||
-||#     # ###### ###### #####   #  ##    # ##    #||
-||#     # #      #      #    #  #  # #   # # #   #||
-||#     # #####  #####  #    #  #  #  #  # #  #  #||
-||#     # #      #      #####   #  #   # # #   # #||
-||#     # #      #      #       #  #    ## #    ##||
-||######  ###### ###### #      ### #     # #     #||
->>================================================<<"""
+ascii_art = """┌───────────────────────────────────────────────────────────────────┐
+│$$\      $$\                                                       │
+│$$$\    $$$ |                                                      │
+│$$$$\  $$$$ | $$$$$$\   $$$$$$\   $$$$$$\   $$$$$$\  $$$$$$\$$$$\  │
+│$$\$$\$$ $$ | \____$$\ $$  __$$\ $$  __$$\  \____$$\ $$  _$$  _$$\ │
+│$$ \$$$  $$ | $$$$$$$ |$$ |  \__|$$ /  $$ | $$$$$$$ |$$ / $$ / $$ |│
+│$$ |\$  /$$ |$$  __$$ |$$ |      $$ |  $$ |$$  __$$ |$$ | $$ | $$ |│
+│$$ | \_/ $$ |\$$$$$$$ |$$ |      \$$$$$$$ |\$$$$$$$ |$$ | $$ | $$ |│
+│\__|     \__| \_______|\__|       \____$$ | \_______|\__| \__| \__|│
+│                                 $$\   $$ |                        │
+│                                 \$$$$$$  |                        │
+│                                  \______/                         │
+└───────────────────────────────────────────────────────────────────┘"""
 
 def load_backend():
     try:
         import torch
         print(ascii_art, file=sys.stderr, flush=True) # print ascii art
-        print("DeepINN version:" + __version__, file=sys.stderr, flush=True) # print DeepINN version
+        print("Margam version:" + __version__, file=sys.stderr, flush=True) # print DeepINN version
         print("Using default backend: PyTorch(",torch.device(0).type,")", file=sys.stderr, flush=True)   # print backend name like an error # https://www.askpython.com/python/python-stdin-stdout-stderr
         print("Pytorch version: ",torch.__version__, file=sys.stderr, flush=True) # print pytorch version
 
